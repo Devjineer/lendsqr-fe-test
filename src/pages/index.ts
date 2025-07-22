ddde65bd5ced6740e7370ca2a5fbf40768c2f37a
Josemaria Ofurum <Josemariaofurum@gmail.com>
2025-07-22 13:21:39 +0100

    set up project with vite, Typescript, scss

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import Login from "./Login";
+import DashBoard from "./DashBoard";
+import UserDetail from "./UserDetail";
+import Users from "./Users";
+
+export { Login, DashBoard, UserDetail, Users };
